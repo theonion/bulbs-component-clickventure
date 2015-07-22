cc4841773b6b3f1ecc173d82c347b6da9bdf0962
Andrew Kos <akos@theonion.com>
2015-07-22 16:35:16 -0500

    Remove clickventure setup, this should happen on a per-site basis

--- src/django-bulbs-public/static/js/clickventure-setup.js
+++
@@ -1,9 +0,0 @@
-(function ($, Clickventure) {
-
-  $(document).ready(function () {
-    var clickventure = new Clickventure('#clickventure-container', {
-      hashState: true
-    });
-  });
-
-})(jQuery, Clickventure);
--- src/django-bulbs/pipeline.py
+++ src/django-bulbs/pipeline.py
@@ -18,7 +18,6 @@ JS = {
             "velocity/jquery.velocity.min.js",
             "velocity/velocity.ui.min.js",
             "js/clickventure.js",
-            "js/clickventure-setup.js"
         ),
         "output_filename": "js/clickventure.js"
     }

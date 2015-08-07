799ed0b8ed29cbc463c1573671c757df2be79970
Andrew Kos <akos@theonion.com>
2015-08-07 11:30:14 -0500

    Remove old indexing functions

--- src/django-bulbs/models.py
+++ src/django-bulbs/models.py
@@ -26,19 +26,3 @@ class ClickventureMixin(models.Model):
 
     class Meta:
         abstract = True
-
-    def to_dict(self):
-        doc = super(ClickventureMixin, self).to_dict()
-        del doc["nodes"]
-        return doc
-
-    def extract_document(self):
-        doc = super(ClickventureMixin, self).extract_document()
-        node_text = [""]  # leading line break
-        for node in self.nodes:
-            node_text.append(node["title"])
-            node_text.append(node["body"])
-            for link in node["links"]:
-                node_text.append(link["body"])
-        doc["body"] += "\n".join(node_text)
-        return doc

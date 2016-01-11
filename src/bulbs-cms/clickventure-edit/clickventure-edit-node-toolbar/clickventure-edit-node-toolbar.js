541a8ee8537eb959ad1124e670c6649725476c94
Andrew Kos <akos123@gmail.com>
2016-01-11 15:27:39 -0600

    sticky node toolbar

--- src/bulbs-cms/clickventure-edit/clickventure-edit-node-toolbar/clickventure-edit-node-toolbar.js
+++ src/bulbs-cms/clickventure-edit/clickventure-edit-node-toolbar/clickventure-edit-node-toolbar.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('bulbs.clickventure.edit.nodeToolbar', [
+  'jquery',
   'bulbs.clickventure.edit.service'
 ])
   .directive('clickventureEditNodeToolbar', [
-    function () {
+    '$',
+    function ($) {
       return {
         restrict: 'E',
         templateUrl: 'clickventure-edit-node-toolbar/clickventure-edit-node-toolbar.html',
@@ -16,7 +18,31 @@ angular.module('bulbs.clickventure.edit.nodeToolbar', [
             $scope.data = ClickventureEdit.getData();
             $scope.changeConfigPage = ClickventureEdit.changeConfigPage;
           }
-        ]
+        ],
+        link: function (scope, elements) {
+          var $nav = $('nav-bar nav');
+
+          $(window).on('scroll', requestAnimationFrame.bind(null, function () {
+            var container = elements.parent();
+
+            if (container[0].getBoundingClientRect().top - $nav.height() <= 0) {
+              var padding = $nav.height();
+
+              elements.css('position', 'fixed');
+              elements.css('top', padding + 'px');
+              elements.css('width', container.width() + 'px');
+              elements.css('z-index', '100');
+
+              container.css('padding-top', padding + 'px')
+            } else {
+              elements.css('position', '');
+              elements.css('top', '');
+              elements.css('width', '');
+
+              container.css('padding-top', '');
+            }
+          }));
+        }
       };
     }
   ]);

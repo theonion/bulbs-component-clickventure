11c1fb9fa2efbeaf8453da94b7a0e77c01a660cb
Andrew Kos <akos123@gmail.com>
2016-01-18 10:56:44 -0600

    fix some styling of edit node container

--- src/bulbs-cms/clickventure-edit/clickventure-edit-node/clickventure-edit-node-container/clickventure-edit-node-container.js
+++ src/bulbs-cms/clickventure-edit/clickventure-edit-node/clickventure-edit-node-container/clickventure-edit-node-container.js
@@ -1,22 +1,24 @@
 angular.module('bulbs.clickventure.edit.node.container', [
   'bulbs.clickventure.edit.service'
 ])
-  .directive('clickventureEditNodeContainer', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'clickventure-edit-node/clickventure-edit-node-container/clickventure-edit-node-container.html',
-      require: '^clickventureEdit',
-      transclude: true,
-      scope: {
-        configPageTitle: '@'
-      },
-      controller: [
-        '$scope', 'ClickventureEdit',
-        function ($scope, ClickventureEdit) {
-          ClickventureEdit.registerConfigPage($scope.configPageTitle);
+  .directive('clickventureEditNodeContainer', [
+    function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'clickventure-edit-node/clickventure-edit-node-container/clickventure-edit-node-container.html',
+        require: '^clickventureEdit',
+        transclude: true,
+        scope: {
+          configPageTitle: '@'
+        },
+        controller: [
+          '$scope', 'ClickventureEdit',
+          function ($scope, ClickventureEdit) {
+            ClickventureEdit.registerConfigPage($scope.configPageTitle);
 
-          $scope.data = ClickventureEdit.getData();
-        }
-      ]
-    };
-  });
+            $scope.data = ClickventureEdit.getData();
+          }
+        ]
+      };
+    }
+  ]);

ec5ca65842267be4379cb906be509b71f54c247f
Andrew Kos <akos123@gmail.com>
2016-01-18 10:13:44 -0600

    added node edit pane container

---
+++ src/bulbs-cms/clickventure-edit/clickventure-edit-node/clickventure-edit-node-container/clickventure-edit-node-container.js
@@ -0,0 +1,22 @@
+angular.module('bulbs.clickventure.edit.node.container', [
+  'bulbs.clickventure.edit.service'
+])
+  .directive('clickventureEditNodeContainer', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'clickventure-edit-node/clickventure-edit-node-container/clickventure-edit-node-container.html',
+      require: '^clickventureEdit',
+      transclude: true,
+      scope: {
+        configPageTitle: '@'
+      },
+      controller: [
+        '$scope', 'ClickventureEdit',
+        function ($scope, ClickventureEdit) {
+          ClickventureEdit.registerConfigPage($scope.configPageTitle);
+
+          $scope.data = ClickventureEdit.getData();
+        }
+      ]
+    };
+  });

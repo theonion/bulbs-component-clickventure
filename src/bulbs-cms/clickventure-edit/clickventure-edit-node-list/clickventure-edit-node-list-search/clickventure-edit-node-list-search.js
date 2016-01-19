b8b2fd06373f26e5491754ccc312032d55797a6f
Andrew Kos <akos123@gmail.com>
2016-01-19 17:54:00 -0600

    rudimentary node list search without results display

--- src/bulbs-cms/clickventure-edit/clickventure-edit-node-list/clickventure-edit-node-list-search/clickventure-edit-node-list-search.js
+++ src/bulbs-cms/clickventure-edit/clickventure-edit-node-list/clickventure-edit-node-list-search/clickventure-edit-node-list-search.js
@@ -1,9 +1,11 @@
 angular.module('bulbs.clickventure.edit.nodeList.search', [
   'autocompleteBasic',
-  'bulbs.clickventure.edit.service'
+  'bulbs.clickventure.edit.service',
+  'lodash'
 ])
   .directive('clickventureEditNodeListSearch', [
-    function () {
+    '_',
+    function (_) {
       return {
         restrict: 'E',
         templateUrl: 'clickventure-edit-node-list/clickventure-edit-node-list-search/clickventure-edit-node-list-search.html',
@@ -13,9 +15,32 @@ angular.module('bulbs.clickventure.edit.nodeList.search', [
           function ($scope, ClickventureEdit) {
 
             $scope.data = ClickventureEdit.getData();
+            $scope.searchTerm = '';
 
-            $scope.search = function (searchTerm) {
-              console.log('searching', searchTerm);
+            $scope.search = _.debounce(function (searchTerm) {
+              var searchTermRE = new RegExp(searchTerm, 'i');
+
+              var results;
+              if (searchTerm) {
+                var results = data.nodes.filter(function (node) {
+                  return node.title.match(searchTermRE) ||
+                    node.body.match(searchTermRE) ||
+                    node.links.filter(function (link) {
+                      return link.body.match(searchTermRE);
+                    }).length > 0;
+                });
+              } else {
+                results = data.nodes;
+              }
+// TODO : make this actually filter node list
+              console.log('searching', searchTerm, results.length);
+            }, 250);
+
+            $scope.searchKeypress = function  (e) {
+              if (e.keyCode === 27) {
+                // esc, clear
+                $scope.searchTerm = '';
+              }
             };
           }
         ]

d168dffdf4f225a9dfd1a8fd4a59e688ec115ce3
Andrew Kos <akos123@gmail.com>
2016-02-01 11:57:22 -0600

    testing harness up, single failing test

---
+++ resources/js/testing/karma.conf.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = function (config) {
+  config.set({
+    basePath: path.join('..', '..', '..'),
+    frameworks: [
+      'chai',
+      'mocha',
+      'sinon'
+    ],
+    files: [
+      path.join('./src', 'bulbs-cms', '**', '*.spec.js')
+    ],
+    browsers: ['Chrome']
+  });
+};
---
+++ src/bulbs-cms/clickventure-edit/clickventure-edit-services/clickventure-edit-config-pages-service.spec.js
@@ -0,0 +1,8 @@
+describe('ClickventureEditConfigPages', function () {
+
+  it('should fail', function () {
+
+    // TODO : add test code here
+    throw new Error('Not implemented yet.');
+  });
+});

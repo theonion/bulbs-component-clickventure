90efd740d99f132c6e1742ea5af40c400af8374d
Andrew Kos <akos123@gmail.com>
2016-02-03 12:09:45 -0600

    fix up some karma settings

--- resources/js/testing/karma.conf.js
+++ resources/js/testing/karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
     files: [
       path.join('bower_components', 'angular', 'angular.js'),
       path.join('bower_components', 'angular-bootstrap', 'ui-bootstrap.js'),
+      path.join('bower_components', 'angular-bootstrap', 'ui-bootstrap-tpls.js'),
       path.join('bower_components', 'angular-mocks', 'angular-mocks.js'),
       path.join('bower_components', 'jquery', 'dist', 'jquery.js'),
       path.join('bower_components', 'lodash', 'lodash.js'),

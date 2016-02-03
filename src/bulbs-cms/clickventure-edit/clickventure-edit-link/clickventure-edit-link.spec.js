5fbbfef4d90b8db25a54a58ddf95a44549530fd8
Andrew Kos <akos123@gmail.com>
2016-02-03 15:34:02 -0600

    placeholders for clickventureEditLink directive

--- src/bulbs-cms/clickventure-edit/clickventure-edit-link/clickventure-edit-link.spec.js
+++ src/bulbs-cms/clickventure-edit/clickventure-edit-link/clickventure-edit-link.spec.js
@@ -28,4 +28,52 @@ describe('clickventureEditLink', function () {
       $directiveScope = element.isolateScope();
     });
   });
+
+  describe('should have a method to delete a node\'s links that', function () {
+
+    it('should delete the link on modal confirm', function () {
+
+      // TODO : add test code here
+      throw new Error('Not implemented yet.');
+    });
+  });
+
+  describe('should have a method to open an add page modal that', function () {
+
+    it('should open an add page modal', function () {
+
+      // TODO : add test code here
+      throw new Error('Not implemented yet.');
+    });
+  });
+
+  describe('should have a method to display node titles that', function () {
+
+    it('should display nodes with order and using the node name filter', function () {
+
+      // TODO : add test code here
+      throw new Error('Not implemented yet.');
+    });
+  });
+
+  describe('should have a method to search nodes by search term that', function () {
+
+    it('should be able to search by node number', function () {
+
+      // TODO : add test code here
+      throw new Error('Not implemented yet.');
+    });
+
+    it('should be able to search by node title', function () {
+
+      // TODO : add test code here
+      throw new Error('Not implemented yet.');
+    });
+
+    it('should return a promise interface', function () {
+
+      // TODO : add test code here
+      throw new Error('Not implemented yet.');
+    });
+  });
 });

9bdaebf25538a50bfbaf5a7fa33401c85fe8d181
Andrew Kos <akos123@gmail.com>
2016-02-08 10:03:50 -0600

    new build

--- compat-builds/django-bulbs-cms/static/cms/clickventure/clickventure.js
+++ compat-builds/django-bulbs-cms/static/cms/clickventure/clickventure.js
@@ -742,13 +742,14 @@ angular.module('bulbs.clickventure.edit.services.node.link.factory', [
   ]);
 
 angular.module('bulbs.clickventure.edit.services.node', [
+  'bulbs.clickventure.edit.nodeNameFilter',
   'bulbs.clickventure.edit.services.node.factory',
   'bulbs.clickventure.edit.services.node.link.factory',
   'lodash'
 ])
   .service('ClickventureEdit', [
-    '_', '$filter', 'ClickventureEditNode',
-    function (_, $filter, ClickventureEditNode) {
+    '_', '$filter', 'ClickventureEditNode', 'ClickventureEditNodeLink',
+    function (_, $filter, ClickventureEditNode, ClickventureEditNodeLink) {
 
       var data = {
         nodeActive: null,
@@ -760,14 +761,12 @@ angular.module('bulbs.clickventure.edit.services.node', [
         select: []
       };
 
-      var _setNodeViewData = function (node, preset) {
+      var _setNodeViewData = function (node) {
         var viewData;
-        var settings = preset || {};
 
         data.view[node.id] = {
           node: node,
-          order: settings.order ||
-            Math.max.apply(null, data.nodes.map(function (node) {
+          order: Math.max.apply(null, data.nodes.map(function (node) {
               if (node.id in data.view) {
                 return data.view[node.id].order;
               }
@@ -828,204 +827,191 @@ angular.module('bulbs.clickventure.edit.services.node', [
         return node;
       };
 
-      var updateInboundLinks = function (link) {
-        if (typeof link.to_node === 'number') {
-          var links = data.view[link.to_node].inboundLinks;
-
-          if (links.indexOf(link.from_node) < 0) {
-            links.push(link.from_node);
-          }
-        }
-      };
-
-      var setNodes = function (nodes) {
-        if (!_.isArray(nodes)) {
-          nodes = [];
-        }
-
-        data.nodes = nodes;
-        data.view = {};
-
-        var newActiveNode = null;
-        if (nodes.length < 1) {
-          // ensure there's at least one node
-          addAndSelectNode();
-        } else {
-          data.nodes.forEach(function (node, i) {
-            // some cleanup to ensure old nodes are in a good state
-            _updateNodeData(node);
-
-            // 1-based index for readability
-            _setNodeViewData(node, {order: i + 1});
-
-            if (i === 0 && data.nodeActive === null ||
-                newActiveNode === null && data.nodeActive.id === node.id) {
-              newActiveNode = node;
-            }
-          });
-
-          // setup inboundLinks
-          data.nodes.forEach(function (node) {
-            node.links.forEach(function (link) {
-              updateInboundLinks(link);
-            });
-          });
-
-          selectNode(newActiveNode);
-        }
-
-        return _reindexNodes();
-      };
-
-      var addNode = function () {
-        var node = new ClickventureEditNode({
-          id: _getNextNodeId(),
-          start: data.nodes.length === 0
-        });
-
-        var activeNodeIndex = data.nodes.indexOf(data.nodeActive);
-        if (activeNodeIndex >= 0) {
-          data.nodes.splice(activeNodeIndex + 1, 0, node);
-        } else {
-          data.nodes.push(node);
-        }
-
-        _setNodeViewData(node);
-        _reindexNodes();
-
-        return node;
-      };
-
-      var addAndSelectNode = function () {
-        return selectNode(addNode());
-      };
-
-      var reorderNode = function (indexFrom, indexTo) {
-        var node = data.nodes[indexFrom];
-
-        if (typeof(indexTo) !== 'number' ||
-            indexTo < 0 ||
-            indexTo >= data.nodes.length) {
-          // don't move it if an invalid index was given
-          data.view[node.id].order = indexFrom + 1;
-          return data.nodes;
-        }
-
-        data.nodes.splice(indexFrom, 1);
-        data.nodes.splice(indexTo, 0, node);
-
-        return _reindexNodes();
-      };
-
-      var registerSelectNodeHandler = function (func) {
-        handlers.select.push(func);
-      };
-
-      var selectNode = function (node) {
-        data.nodeActive = node;
-
-        handlers.select.forEach(function (func) {
-          func(node);
-        });
-
-        return node;
-      };
-
-      var cloneNode = function (node) {
-        var clonedNode = addAndSelectNode();
-
-        _.assign(
-          clonedNode,
-          _.omit(node, [
-            'id',
-            'links',
-            'statuses',
-            'sister_pages',
-            'title'
-          ])
-        );
-
-        clonedNode.title = 'Clone - ' + $filter('clickventure_node_name')(node);
-
-        // so we don't modify the original page's links
-        clonedNode.links = node.links.map(function (link) {
-          var newLink = _.clone(link);
-
-          newLink.from_node = clonedNode.id;
-          updateInboundLinks(newLink);
-
-          return newLink;
-        });
-
-        clonedNode.statuses = _.clone(node.statuses);
-
-        // this node gets the current list of siblings (which doesn't include itself)
-        clonedNode.sister_pages = _.clone(node.sister_pages);
-
-        // tell each sister that they have a new sibling
-        node.sister_pages.forEach(function (sisterId) {
-          var sister = data.view[sisterId].node;
-
-          if (!_.isArray(sister.sister_pages)) {
-            sister.sister_pages = [];
-          }
-
-          sister.sister_pages.push(clonedNode.id);
-        });
-
-        // tell the new siblings about eachother
-        clonedNode.sister_pages.push(node.id);
-        node.sister_pages.push(clonedNode.id);
-
-        return clonedNode;
-      };
-
-      var deleteNode = function (rmNode) {
-        var i = data.nodes.indexOf(rmNode);
-
-        if (i >= 0) {
-          data.nodes.splice(i, 1);
-          delete data.view[rmNode.id];
-
-          data.nodes.forEach(function (node) {
-            node.links.forEach(function (link) {
-              if (typeof link.to_node === 'number' && link.to_node === rmNode.id) {
-                link.to_node = null;
-              }
-            });
-
-            node.sister_pages = _.without(node.sister_pages, rmNode.id);
-            data.view[node.id].inboundLinks = _.without(data.view[node.id].inboundLinks, rmNode.id);
-          });
-        }
-
-        if (data.nodes.length < 1) {
-          // ensure there's at least 1 node
-          addAndSelectNode();
-        }
-
-        var nextNodeId = i - 1;
-        if (nextNodeId < 0) {
-          nextNodeId = 0;
-        }
-
-        selectNode(data.nodes[nextNodeId]);
-
-        return _reindexNodes();
-      };
-
       return {
+        addNode: function () {
+          var node = new ClickventureEditNode({
+            id: _getNextNodeId(),
+            start: data.nodes.length === 0
+          });
+
+          var activeNodeIndex = data.nodes.indexOf(data.nodeActive);
+          if (activeNodeIndex >= 0) {
+            data.nodes.splice(activeNodeIndex + 1, 0, node);
+          } else {
+            data.nodes.push(node);
+          }
+
+          _setNodeViewData(node);
+          _reindexNodes();
+
+          return node;
+        },
+        addAndSelectNode: function () {
+          return this.selectNode(this.addNode());
+        },
+        updateInboundLinks: function (link) {
+          if (typeof link.to_node === 'number') {
+            var links = data.view[link.to_node].inboundLinks;
+
+            if (links.indexOf(link.from_node) < 0) {
+              links.push(link.from_node);
+            }
+          }
+        },
         getData: function () {
           return data;
         },
-        setNodes: setNodes,
-        addNode: addNode,
-        addAndSelectNode: addAndSelectNode,
-        reorderNode: reorderNode,
-        registerSelectNodeHandler: registerSelectNodeHandler,
-        selectNode: selectNode,
-        cloneNode: cloneNode,
-        deleteNode: deleteNode,
+        setNodes: function (nodes) {
+          data.nodes = [];
+          data.view = {};
+
+          if (!_.isArray(nodes)) {
+            nodes = [];
+          }
+
+          var newActiveNode = null;
+          if (nodes.length < 1) {
+            // ensure there's at least one node
+            this.addAndSelectNode();
+          } else {
+            nodes.forEach(function (node, i) {
+              data.nodes.push(new ClickventureEditNode(node));
+
+              // some cleanup to ensure old nodes are in a good state
+              _updateNodeData(node);
+
+              // 1-based index for readability
+              _setNodeViewData(node);
+
+              if (i === 0 && data.nodeActive === null ||
+                  newActiveNode === null && data.nodeActive.id === node.id) {
+                newActiveNode = node;
+              }
+            });
+
+            // setup inboundLinks
+            var _this = this;
+            data.nodes.forEach(function (node) {
+              node.links.forEach(function (link) {
+                _this.updateInboundLinks(link);
+              });
+            });
+
+            this.selectNode(newActiveNode);
+          }
+
+          return _reindexNodes();
+        },
+        reorderNode: function (indexFrom, indexTo) {
+          var node = data.nodes[indexFrom];
+
+          if (typeof indexTo !== 'number' ||
+              indexTo < 0 ||
+              indexTo >= data.nodes.length) {
+            // don't move it if an invalid index was given
+            data.view[node.id].order = indexFrom + 1;
+            return data.nodes;
+          }
+
+          data.nodes.splice(indexFrom, 1);
+          data.nodes.splice(indexTo, 0, node);
+
+          return _reindexNodes();
+        },
+        registerSelectNodeHandler: function (func) {
+          handlers.select.push(func);
+        },
+        selectNode: function (node) {
+          data.nodeActive = node;
+
+          handlers.select.forEach(function (func) {
+            func(node);
+          });
+
+          return node;
+        },
+        cloneNode: function (node) {
+          var clonedNode = this.addAndSelectNode();
+
+          _.assign(
+            clonedNode,
+            _.omit(node, [
+              'id',
+              'links',
+              'statuses',
+              'sister_pages',
+              'title'
+            ])
+          );
+
+          clonedNode.title = 'Clone - ' + $filter('clickventure_node_name')(node);
+
+          // so we don't modify the original page's links
+          var _this = this;
+          clonedNode.links = node.links.map(function (link) {
+            var newLink = new ClickventureEditNodeLink(link);
+
+            newLink.from_node = clonedNode.id;
+            _this.updateInboundLinks(newLink);
+
+            return newLink;
+          });
+
+          clonedNode.statuses = _.clone(node.statuses);
+
+          // this node gets the current list of siblings (which doesn't include itself)
+          clonedNode.sister_pages = _.clone(node.sister_pages);
+
+          // tell each sister that they have a new sibling
+          node.sister_pages.forEach(function (sisterId) {
+            var sister = data.view[sisterId].node;
+
+            if (!_.isArray(sister.sister_pages)) {
+              sister.sister_pages = [];
+            }
+
+            sister.sister_pages.push(clonedNode.id);
+          });
+
+          // tell the new siblings about eachother
+          clonedNode.sister_pages.push(node.id);
+          node.sister_pages.push(clonedNode.id);
+
+          return clonedNode;
+        },
+        deleteNode: function (rmNode) {
+          var i = data.nodes.indexOf(rmNode);
+
+          if (i >= 0) {
+            data.nodes.splice(i, 1);
+            delete data.view[rmNode.id];
+
+            data.nodes.forEach(function (node) {
+              node.links.forEach(function (link) {
+                if (typeof link.to_node === 'number' && link.to_node === rmNode.id) {
+                  link.to_node = null;
+                }
+              });
+
+              node.sister_pages = _.without(node.sister_pages, rmNode.id);
+              data.view[node.id].inboundLinks = _.without(data.view[node.id].inboundLinks, rmNode.id);
+            });
+          }
+
+          if (data.nodes.length < 1) {
+            // ensure there's at least 1 node
+            this.addAndSelectNode();
+          }
+
+          var nextNodeId = i - 1;
+          if (nextNodeId < 0) {
+            nextNodeId = 0;
+          }
+
+          this.selectNode(data.nodes[nextNodeId]);
+
+          return _reindexNodes();
+        },
         getValidLinkStyles: function () {
           return [
             '',
@@ -1033,16 +1019,6 @@ angular.module('bulbs.clickventure.edit.services.node', [
             'Dialogue',
             'Music',
             'Quiz'
-          ];
-        },
-        getValidNodeTransitions: function () {
-          return [
-            'default',
-            'slideLeft',
-            'slideRight',
-            'slideUp',
-            'slideDown',
-            'flipLeft'
           ];
         },
         addLink: function (node) {
@@ -1055,7 +1031,6 @@ angular.module('bulbs.clickventure.edit.services.node', [
 
           return link;
         },
-        updateInboundLinks: updateInboundLinks,
         reorderLink: function (node, indexFrom, indexTo) {
           if (indexFrom >= 0 && indexTo >= 0 && indexTo < node.links.length) {
             var link = node.links[indexFrom];
@@ -1065,10 +1040,16 @@ angular.module('bulbs.clickventure.edit.services.node', [
         },
         deleteLink: function (node, rmLink) {
           var indexLinks = node.links.indexOf(rmLink);
+          var toNode = rmLink.to_node;
+
           node.links.splice(indexLinks, 1);
 
-          if (typeof rmLink.to_node !== 'number') {
-            var linksInbound = data.view[rmLink.to_node].inboundLinks;
+          var hasAnotherLinkToNode = typeof node.links.find(function (link) {
+            return link.to_node === toNode;
+          }) !== 'undefined';
+
+          if (typeof toNode === 'number' && !hasAnotherLinkToNode) {
+            var linksInbound = data.view[toNode].inboundLinks;
             var indexInbound = linksInbound.indexOf(rmLink.from_node);
             linksInbound.splice(indexInbound, 1);
           }
